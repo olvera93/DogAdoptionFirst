bec481fcdde4039962aefd248f8eb576ed30dc9c
olvera93 <olvera9302@outlook.com>
2023-04-24 14:39:17 -0600

    [ADD] Skeleton

---
+++ app/src/main/java/com/olvera/dogadoptionfirst/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.olvera.dogadoptionfirst
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.olvera.dogadoptionfirst.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/olvera/dogadoptionfirst/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.olvera.dogadoptionfirst
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {}
